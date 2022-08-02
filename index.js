f2becc65a97e804d1f74523f2577fe896ad60167
Evans Ochieng <evansochieng121@gmail.com>
2022-08-02 16:54:49 +0300

    Completed learning

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Follow along with the examples here
+function sayHello(name){
+    console.log(`Hello, ${name}!`)
+}
+sayHello('Beryl');
+sayHello('Mariko');
+sayHello('Isabela')
+
+function add(x, y){
+    return x + y;
+}
+
+console.log(add(1, 2))
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner"));
